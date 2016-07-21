d7bfe3197bf1af5f5e103ef8eb79a990243dbaaf
ShaimaaAbdelhamed <shaimaa@yahoo.com>
2016-07-21 11:17:14 +0300

    Xunitadd

---
+++ DateLog/xUnit/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace xUnit
+{
+    public class Class1
+    {
+        [Fact]
+        public void WriteTxt_null_false()
+        {
+            report re = new report();
+            Assert.Equal(re.Writetxt("hi", null), true);
+        }
+    }
+}
